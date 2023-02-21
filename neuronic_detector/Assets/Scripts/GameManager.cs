a7f8c49ab212fb317b853783ce43203540877952
Irene Geng <irenegeng2@berkeley.edu>
2023-02-21 14:19:07 -0800

    added placeholder main menu scene

--- neuronic_detector/Assets/Scripts/GameManager.cs
+++ neuronic_detector/Assets/Scripts/GameManager.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance = null;
+
+    private void Awake()
     {
-        
+        if (Instance == null) {
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("TutorialScene");
     }
 }
